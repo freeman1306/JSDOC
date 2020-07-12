65ee72a3e16ea3f9f3b2dfa9f08dbea0b6f45fab
freeman1306 <zavalkin06@gmail.com>
2020-07-12 21:40:47 +0300

    process

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @type {string}
+ */
+const studentName = 'John Doe'
+
+
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [12, 34, 45, 43.3]
+
+
+
+/**
+ * @type {{id: number|string, text: string}}
+ */
+const todo = {
+    id: 1,
+    text: 'hello'
+}
